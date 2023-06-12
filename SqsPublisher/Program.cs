bc8ae2ca2e3e3dabc5dd062481fabe1047875386
Leonidas Antoniou <antoniou.leo@gmail.com>
2023-06-12 17:21:53 +0100

    Created SQS Publisher & Consumer
    SqsPublisher publishes messages to the sqs service.
    SqsConsumer receives the messages.

---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    Email = "leo@leo.com",
+    FullName = "Leo Antoniou",
+    DateOfBirth = new DateTime(1996, 07, 27),
+    GithubUsername = "leo9806",
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated)
+            }
+        }
+    }
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
