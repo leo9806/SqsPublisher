1b1577b565d776d34974e439306a49a406bb1912
Leonidas Antoniou <antoniou.leo@gmail.com>
2023-06-15 15:56:29 +0100

    Created a customer consumer
    Uses mediatr to process/handle the messages

---
+++ Customers.Consumer/Program.cs
@@ -0,0 +1,13 @@
+using Amazon.SQS;
+using Customers.Consumer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<QueueSettings>(builder.Configuration.GetSection(QueueSettings.Key));
+builder.Services.AddSingleton<IAmazonSQS, AmazonSQSClient>();
+builder.Services.AddHostedService<QueueConsumerService>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
+
+var app = builder.Build();
+
+app.Run();
